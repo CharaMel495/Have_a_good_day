31b25e62bc136743e4be66973a7e57cbf36ab8b5
CharaMel495 <135007499+CharaMel495@users.noreply.github.com>
2026-06-29 18:17:53 +0900

    タイトル改修

---
+++ Assets/Scripts/DataAssets/ScoreBoardData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ScoreBoardDataAsset", fileName = "ScoreBoardDataAsset")]
+public class ScoreBoardDataAsset : ScriptableObject
+{
+    public int[] Scores;
+
+    public string GetScoreText(int ranking)
+    {
+        return ranking switch
+        {
+            0 => $"1st:{Scores[0]:'0000'}pts",
+            1 => $"2nd:{Scores[1]:'0000'}pts",
+            2 => $"3rd:{Scores[2]:'0000'}pts",
+            3 => $"4th:{Scores[3]:'0000'}pts",
+            4 => $"5th:{Scores[4]:'0000'}pts",
+            _ => ""
+        };
+    }
+}
+
