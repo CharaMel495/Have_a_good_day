ef29c08f13791e8d210ee82ec1d6b829293cc705
Charamel495 <to_1525@yahoo.co.jp>
2026-07-01 00:34:26 +0900

    スコアの基盤作成

--- Assets/Scripts/Score/ScoreManager.cs
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -7,25 +7,51 @@ public class ScoreManager : SingletonMonoBehaviour<ScoreManager>
     [SerializeField]
     private ScoreDataAsset _scoreData;
     [SerializeField]
-    private int _comboKeepTime;
+    private float _comboKeepTime;
     private int _currentCombo;
     private int _maxCombo;
+    private float _remainComboKeepTime;
 
     // Start is called before the first frame update
     public void Initialize()
     {
-           
+        _currentCombo = 0;
+        _maxCombo = 0;
+
+        _scoreData.ComboCount = 0;
+        _scoreData.EggValue = 10;
+        _scoreData.WingFrappCount = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentCombo > 0)
+            JudgeComboKeep();
+    }
+
+    private void JudgeComboKeep()
+    {
+        _remainComboKeepTime -= Time.fixedDeltaTime;
+
+        if (_remainComboKeepTime > 0)
+            return;
+
+        _currentCombo = 0;
     }
 
     [CallableEvent("OnGimmickEliminated")]
     public void OnGimmickEliminated(object _)
     {
+        ++_currentCombo;
+        _remainComboKeepTime = _comboKeepTime;
 
+        if (_currentCombo > _maxCombo)
+            _maxCombo = _currentCombo;
     }
 
     [CallableEvent("OnWingFlapped")]
     public void OnWingFlapped()
     {
-
+        ++_scoreData.WingFrappCount;
     }
 }
